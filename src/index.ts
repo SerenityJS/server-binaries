30f273584f53aacd1aae878e5a2a751888647937
Payton Kerby <paytonmk744@outlook.com>
2025-01-23 14:42:13 -0600

    chore: added module injection

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Serenity, LevelDBProvider } from "@serenityjs/core";
+import { Pipeline } from "@serenityjs/plugins";
+
+import { Modules } from "./modules"
+
+// Inject all modules
+Bun.plugin({
+  name: "SerenityJS Native Module Injection",
+  setup(build) {
+    // Inject all modules
+    for (const inject of Modules) inject(build);
+  },
+});
+
+// Create a new Serenity instance
+const serenity = new Serenity({
+  path: "./properties.json",
+  serenity: {
+    permissions: "./permissions.json",
+    resourcePacks: "./resource_packs"
+  }
+});
+
+// Create a new plugin pipeline
+new Pipeline(serenity, { path: "./plugins" });
+
+// Register the LevelDBProvider
+serenity.registerProvider(LevelDBProvider, { path: "./worlds" });
+
+// Start the server
+serenity.start();
